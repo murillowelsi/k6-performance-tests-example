b9d7f481078e8fa914f589f7d47b646780795231
Murillo Welsi <murillo.welsi@gmail.com>
2021-09-28 22:15:53 +0100

    Tests 2

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check, group, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
+    { duration: '30s', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    { duration: '60s', target: 100 }, // stay at 100 users for 10 minutes
+    { duration: '30s', target: 0 }, // ramp-down to 0 users
   ],
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
 };
 
-export default function () {
-  let res = http.get('https://app-vlc.hotmart.com/login');
-  check(res, { 'status was 200': (r) => r.status == 200 });
+const BASE_URL = 'https://test-api.k6.io';
+const USERNAME = 'TestUser';
+const PASSWORD = 'SuperCroc2020';
+
+export default () => {
+  let loginRes = http.post(`${BASE_URL}/auth/token/login/`, {
+    username: USERNAME,
+    password: PASSWORD,
+  });
+
+  check(loginRes, {
+    'logged in successfully': (resp) => resp.json('access') !== '',
+  });
+
+  let authHeaders = {
+    headers: {
+      Authorization: `Bearer ${loginRes.json('access')}`,
+    },
+  };
+
+  let myObjects = http.get(`${BASE_URL}/my/crocodiles/`, authHeaders).json();
+  check(myObjects, { 'retrieved crocodiles': (obj) => obj.length > 0 });
+
   sleep(1);
-}
+};
